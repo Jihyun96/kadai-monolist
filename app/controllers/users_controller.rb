9eba3b61cbb2542d80cc32bf55f1ab7b7280edd7
jh.p <ji_hyun@keio.jp>
2019-01-04 08:58:29 +0000

    change group(:name)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @items = @user.items.group(:name)
+    @items = @user.items.uniq
     @count_want = @user.want_items.count
     @count_have = @user.have_items.count
   end
